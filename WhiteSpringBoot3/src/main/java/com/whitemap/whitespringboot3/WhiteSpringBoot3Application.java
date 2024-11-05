81f3a89d05fa461670ac4f36521a8d7345939422
Regulus2005 <206465633@qq.com>
2024-11-06 03:05:13 +0800

    Initial commit

---
+++ WhiteSpringBoot3/src/main/java/com/whitemap/whitespringboot3/WhiteSpringBoot3Application.java
@@ -0,0 +1,13 @@
+package com.whitemap.whitespringboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteSpringBoot3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhiteSpringBoot3Application.class, args);
+    }
+
+}
---
+++ WhiteSpringBoot3/src/test/java/com/whitemap/whitespringboot3/WhiteSpringBoot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whitemap.whitespringboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteSpringBoot3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
